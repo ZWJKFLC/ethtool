c61fbc1ecd3fc2a4fd95f899c01db2c7156f2721
zwjkf <503755414@qq.com>
2023-12-18 18:45:53 +0800

    update

--- hardhattool/index.js
+++ hardhattool/index.js
@@ -1,5 +1,11 @@
 module.exports = {
     // test,
+    // hardhattool: {
+    //     ...require('./hh_log'),
+    //     ...require('./readcontracts'),
+    //     ...require('./id-readcontracts'),
+    // },
     ...require('./hh_log'),
-    hardhattool:require('./readcontracts'),
+    ...require('./readcontracts'),
+    ...require('./id-readcontracts'),
 }
